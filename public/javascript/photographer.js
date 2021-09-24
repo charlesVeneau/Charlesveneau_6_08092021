9cfc08995cdbc8ed1106ec882d9e9c2cec341e17
CharlesVeneau <veneauc@hotmail.fr>
2021-09-24 11:36:50 +0200

    setup header photographer page

--- public/javascript/photographer.js
+++ public/javascript/photographer.js
@@ -1,5 +1,6 @@
 //retrieve the id of the photagrapher that is stored in the URL params
 const urlId = new URLSearchParams(window.location.search).get("id");
+//Create a variable to store the photographer's information
 var photographer;
 
 fetch("../../assets/fishEyeData.json")
@@ -11,9 +12,15 @@ fetch("../../assets/fishEyeData.json")
   .then(function (value) {
     photographer = value.photographers.filter(function (photographer) {
       return photographer.id == urlId;
-    });
+    })[0];
+    //put the photographer name in the head title
+    document.title += ` | ${photographer.name}`;
+
+    createHeader(photographer);
     console.log(photographer);
   })
   .catch(function (error) {
     console.log(error);
   });
+
+function createHeader(photographer) {}
